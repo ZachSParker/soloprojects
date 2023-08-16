fb252ea5460707c3257e12809170a0db05e7e28c
ZachSParker <evergreenzach@gmail.com>
2023-08-16 16:34:31 -0600

    added hideMole and moleAppear func

--- Whack-a-Mole-ZP/script.js
+++ Whack-a-Mole-ZP/script.js
@@ -1,8 +1,20 @@
+// this function resets the points value to 0
 function pointsClear(id){
     var element = document.querySelector(`#${id}`)
-    element.innerText++;
+    element.innerText = 0;
+    
 }
+//this function increments the points value
 function pointsUp(id){
     var element = document.querySelector(`#${id}`)
     element.innerText++;
+}
+function hideMole(id){
+    let element = document.querySelector(`#${id}`)
+    element.style.display ="none";
+    setTimeout(moleAppear,300,`#${id}`)
+}
+function moleAppear(id){
+    let element = document.querySelector(`#${id}`)
+    element.style.display ="block";
 }
