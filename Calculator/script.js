5e6089814ecde5c69e6496f400cb233a7c92f66b
ZachSParker <evergreenzach@gmail.com>
2023-08-23 18:38:54 -0600

    added functionality to the operators

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,20 +1,50 @@
-
+let num1 = "";
+let num2 = "";
+let operator = "";
 function setOP(element)
-{
+{   
+    var deciOp = ""
+    if(operator =="."){
+        deciOp = num1 + ".";
+        num1 = deciOp;
+    }
+    operator = element;
+    num2 = num1;
+    
+    num1 = "";
     
 }
 function press(element)
 {
     calcDisplay = document.querySelector('#display');
-    calcDisplay.innerHTML +=`${element}`
-    console.log(calcDisplay);
+
+    if(num1 == ""){
+        calcDisplay.innerText = element;
+    }
+    else{
+        calcDisplay.innerText += element;
+    }
+    num1 = calcDisplay.innerText;
     return calcDisplay;
 }
 function calculate()
 {
-
+    sum = 0;
+    if(operator=="x"){
+        sum = num1 * num2;
+    }
+    else if(operator == "/"){
+        sum = num2 / num1;
+    }
+    else if(operator == "+"){
+        sum = num2 += num1;
+    }
+    calcDisplay.innerText = sum;
 }
 function clr()
 {
     calcDisplay.innerHTML = 0;
+    num1 = "";
+    num2 = "";
+    operator = "";
 }
